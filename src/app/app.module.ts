ea453bccc5ad9c7d26d4671fe91f8baf29177e49
pauline <pauline.feytel@gmail.com>
2018-09-07 10:52:07 +0200

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { ListComponent } from './goal/list/list.component';
+import { AddComponent } from './goal/add/add.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    ListComponent,
+    AddComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
