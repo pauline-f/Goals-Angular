b9affa58fc3d3e6402ea33ba3aca699e346ad011
pauline <pauline.feytel@gmail.com>
2018-09-10 10:19:04 +0200

    Create Goal model and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,19 @@ import { SigninComponent } from './auth/signin/signin.component';
 import { ListComponent } from './goal/list/list.component';
 import { AddComponent } from './goal/add/add.component';
 import { HeaderComponent } from './header/header.component';
+import { AuthService } from './services/auth.service';
+import { GoalService } from './services/goal.service';
+import { AuthGuardService } from './services/auth-guard.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: 'auth/signup', component: SignupComponent },
+  { path: 'auth/signin', component: SigninComponent },
+  { path: 'goal/list', component: ListComponent },
+  { path: 'goal/add', component: AddComponent}
+];
 
 @NgModule({
   declarations: [
@@ -20,9 +32,9 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule, RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [AuthService, GoalService, AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/models/Goal.model.ts
@@ -0,0 +1,5 @@
+export class Goal {
+    constructor (public goal: string) {
+        
+    }
+}
